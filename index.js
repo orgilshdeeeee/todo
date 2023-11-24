01e27664482bae60acdbcf3fcd5ee52ed7c54b95
Ariunbileg Orgil <23LP2150@G304-CC0029.local>
2023-11-24 19:28:29 +0800

    test to do

---
+++ index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  const TaskBar = document.getElementById("taskBar");
+  TaskBar.style.removeProperty("dislpay");
+});
